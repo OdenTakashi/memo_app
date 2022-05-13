5dcd11cfac566bfe93718fe46e031050b97bf58a
OdenTakashi <odentakashi@icloud.com>
2022-05-13 16:46:26 +0900

    クラス作成

--- app.rb
+++ app.rb
@@ -6,41 +6,65 @@ require 'json'
 require 'pry'
 require 'pg'
 
-before do
-  @conn = PG.connect(dbname: 'memos')
+class Memo 
+  def initialize
+    @conn = PG.connect(dbname: 'memos')
+  end
+
+  def create(title, content)
+    @conn.exec("INSERT INTO memos (title, content) VALUES('#{title}', '#{content}')")
+  end
+
+  def update(title, content, id)
+    @conn.exec("UPDATE memos SET title ='#{title}', content ='#{content}' WHERE id =#{id}")
+  end
+
+  def select(id)
+    @conn.exec("SELECT * FROM memos WHERE id = #{id}")
+  end
+
+  def delete(id)
+    @conn.exec("DELETE FROM memos WHERE id = '#{id}'")
+  end
+
+  def get
+    @conn.exec('SELECT * FROM memos')
+  end
 end
 
 helpers do
   def h(text)
     Rack::Utils.escape_html(text)
   end
-
-  def select(id)
-    @conn.exec("SELECT * FROM memos WHERE id = #{id}")
-  end
 end
 
 get '/memos' do
-  @memos = @conn.exec('SELECT * FROM memos')
+  detabase = Memo.new
+  @memos = detabase.get
   erb :index
 end
 
 get '/memos/new' do
+  @memos = Memo.new
+  @memos.create("#{params['title']}", "#{params['content']}")
   erb :new
 end
 
 patch '/memos/:id' do
-  @conn.exec("UPDATE memos SET title ='#{params['title']}', content ='#{params['content']}' WHERE id =#{params['id']}")
+  detabase = Memo.new
+  @memos = detabase.update(params['title'], params['content'], params['id'])
   redirect("/memos/#{params['id']}")
 end
 
 get '/memos/:id' do
-  @memos = select(params['id'])
+  detabase = Memo.new
+  @memos = detabase.select(params['id'])
   erb :detail
 end
 
 post '/memos' do
-  @conn.exec("INSERT INTO memos (title, content) VALUES('#{params['title']}', '#{params['content']}')")
+  detabase = Memo.new
+  @memos = detabase.create(params['title'], params['content'])
   redirect('/memos')
 end
 
@@ -49,11 +73,13 @@ get '/memos/file_not_found' do
 end
 
 get '/memos/:id/edit' do
-  @memos = select(params['id'])
+  detabase = Memo.new
+  @memos = detabase.select(params['id'])
   erb :edit
 end
 
 delete '/memos/:id' do
-  @conn.exec("DELETE FROM memos WHERE id = '#{params['id']}'")
+  detabase = Memo.new
+  @memos = detabase.delete(params["id"])
   redirect('/memos')
 end
